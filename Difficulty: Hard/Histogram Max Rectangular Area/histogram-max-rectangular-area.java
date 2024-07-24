f33ffb196a807d3feee9f9d0752f0943ac2241be
Sahil Raut <98373976+sahilraut05@users.noreply.github.com>
2024-07-24 23:26:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    long n = Long.parseLong(br.readLine().trim());
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    long[] arr = new long[(int)n];
+		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
+		    System.out.println(new Solution().getMaxArea(arr, n));
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    //Function to find largest rectangular area possible in a given histogram.
+    public static long getMaxArea(long[] heights, long n) {
+        int length = (int) n;
+        long maxA = 0;
+        int[] nsl = new int[length];
+        int[] nsr = new int[length];
+
+        // next smaller right
+        Stack<Integer> s = new Stack<>();
+        for (int i = length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // next smaller left
+        s = new Stack<>();
+        for (int i = 0; i < length; i++) {
+            while (!s.isEmpty() && heights[s.peek()] >= heights[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // area
+        for (int i = 0; i < length; i++) {
+            int width = nsr[i] - nsl[i] - 1;
+            long currArea = heights[i] * width;
+            maxA = Math.max(currArea, maxA);
+        }
+        return maxA;
+    }
+}
+
+
+
